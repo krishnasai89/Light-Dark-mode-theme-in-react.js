de3fbfb1dcf1620865a0ff18e17cdaad6fafd274
vellampallikrishnasaigifhub <krishnasaivellampalli@gmail.com>
2024-10-21 17:14:14 +0530

    new changes

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import './Navbar.css'
+import logo_light from '../../assets/logo-black.png'
+import logo_dark from '../../assets/logo-white.png'
+import search_icon from '../../assets/search-w.png'
+import search_icon_light from '../../assets/search-w.png'
+import search_icon_dark from '../../assets/search-b.png'
+import toogle_light from '../../assets/night.png'
+import toogle_dark from '../../assets/day.png'
+
+
+const Navbar = ({theme,setTheme}) => {
+  const toggle_mode = () =>{
+    theme == 'light'? setTheme('dark') : setTheme('light');
+  }
+  return (
+    <div className='navbar'>
+        <img src={theme === 'light'? logo_light : logo_dark} alt="logo" className='logo' />
+        <ul>
+            <li>Home</li>
+            <li>Products</li>
+            <li>Features</li>
+            <li>About</li>
+        </ul>
+
+        <div className="Search-box">
+            <input type="text" placeholder='Search'/>
+            <img src={theme === 'light'? search_icon_light : search_icon_dark} alt="Search_icon" />
+        </div>
+        <img src={theme === 'light'? toogle_light : toogle_dark} alt="toggle-icon" className='toggle-icon' onClick={()=>{toggle_mode()}}/>
+    </div>
+  )
+}
+
+export default Navbar
